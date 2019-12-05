c036021013ada0f0702d6c13bd5e136712d15355
taruc <theehj-wm17@student.tarc.edu.my>
2019-12-05 10:37:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/likedislike/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.likedislike
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.prefs.Preferences
+
+class MainActivity : AppCompatActivity() {
+    //Module-level variable
+    //var like: Int = 0
+    //var dislike: Int = 0
+    //lateinit means initialize later
+    lateinit var counterViewModel: CounterViewModel
+    lateinit var  sharedPreferences: SharedPreferences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //d means debug
+        Log.d("MainActivity", "onCreate")
+
+        //Initialize the counterViewModel
+        counterViewModel = ViewModelProviders.of(this).get(CounterViewModel::class.java)
+
+        //Initialize the shared preferences
+        //use getPreferences not sharedPrefences because just on activity
+        sharedPreferences = getPreferences(Context.MODE_PRIVATE)
+
+        imageViewLike.setOnClickListener {
+           counterViewModel.incrementLike()
+           textViewLike.text = counterViewModel.likeCount.toString()
+        }
+
+        imageViewDislike.setOnClickListener{
+            counterViewModel.incrementDislike()
+            textViewDislike.text = counterViewModel.dislikeCount.toString()
+        }
+    }
+
+    override fun onStart() {
+        Log.d("MainActivity", "onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d("MainActivity", "onResume")
+        val like = sharedPreferences.getInt(getString(R.string.like),0)
+        val dislike = sharedPreferences.getInt(getString(R.string.dislike),0)
+        counterViewModel.likeCount = like
+        counterViewModel.dislikeCount = dislike
+
+        textViewLike.text = counterViewModel.likeCount.toString()
+        textViewDislike.text = counterViewModel.dislikeCount.toString()
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d("MainActivity", "onPause")
+        with(sharedPreferences.edit()){
+            putInt(getString(R.string.like), counterViewModel.likeCount)
+            putInt(getString(R.string.dislike), counterViewModel.dislikeCount)
+            apply() //or commit()
+        }
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d("MainActivity", "onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d("MainActivity", "onDestroy")
+        super.onDestroy()
+    }
+}
